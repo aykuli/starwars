68bc772ca4f8eed8a1481035bb0de772f044f568
Aykuli <aykuli@ya.ru>
2020-02-01 16:08:23 +0700

    feat: add components folders

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './app.css';
+
+import Header from '../header';
+
+export default function App() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './header.css';
+
+export default function Header() {
+    return (
+        <h1>header</h1>
+    )
+}
