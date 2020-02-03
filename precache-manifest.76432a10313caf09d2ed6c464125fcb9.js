eea41c4a0935b0dfa5917ec0c4f277577ab92139
Aykuli <aykuli@ya.ru>
2020-02-03 12:27:26 +0700

    feat: add homepage in package.json

---
+++ precache-manifest.76432a10313caf09d2ed6c464125fcb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5d33399af9a00da54f76b39060f795",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af8d8a9a743ba690521f",
+    "url": "./static/css/main.c3cdaac6.chunk.css"
+  },
+  {
+    "revision": "4190a78dcbd3b1613da6",
+    "url": "./static/js/2.d754e9c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.d754e9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8d8a9a743ba690521f",
+    "url": "./static/js/main.b67e06b5.chunk.js"
+  },
+  {
+    "revision": "cb7956d1299acbbf46bf",
+    "url": "./static/js/runtime-main.85ce7624.js"
+  },
+  {
+    "revision": "57a744b1dc37feb5856a5b123c869e72",
+    "url": "./static/media/boom.57a744b1.svg"
+  }
+]);
